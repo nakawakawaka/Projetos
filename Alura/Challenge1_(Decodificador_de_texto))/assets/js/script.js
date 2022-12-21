4044654b11c80109a22a6a0f91c21367d0f6542c
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2022-12-22 00:04:54 +0900

    inserindo a funcao de criptografar texto

---
+++ Alura/Challenge1_(Decodificador_de_texto))/assets/js/script.js
@@ -0,0 +1,53 @@
+const input = document.querySelector("#texto");
+let btnCriptografar = document.querySelector('.criptografar');
+
+btnCriptografar.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(input.value)
+    if (!input.value) {
+        const erro = 'ERRO: Insira um texto para ser criptografado';
+        const label = document.querySelector('label');
+
+        label.innerHTML = erro;
+    } else {
+        const res = document.querySelector('.resultado');
+        const textCrip = criptografar(input.value);
+        const conteudo = criaElemento(textCrip);
+        res.appendChild(conteudo);
+        input.value = '';
+        input.focus();
+    }
+});
+
+function criaElemento(el) {
+    const div = document.createElement('div');
+    const content = document.createTextNode(el);
+    return div.appendChild(content);
+}
+
+function criptografar (conteudo) {
+    const array = conteudo.split('');
+    const newArray = [];
+    for (const el of array) {
+        switch (el) {
+            case 'a':
+                newArray.push('ai');
+                break;
+            case 'e':
+                newArray.push('enter');
+                break;
+            case 'i':
+                newArray.push('imes');
+                break;
+            case 'o':
+                newArray.push('ober');
+                break;
+            case 'u':
+                newArray.push('ufat');
+                break;  
+            default:    
+                newArray.push(el);  
+        }
+    }
+    return newArray.join('');
+}
