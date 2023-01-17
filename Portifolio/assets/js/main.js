f143d61a4cdc8e9035a6df711a44fb56f4bb27e1
naoki nakamura <bruno.naoki.nakamura@gmail.com>
2023-01-17 21:18:43 +0900

    configurando setup do back-end

--- Portifolio/assets/js/main.js
+++ Portifolio/assets/js/main.js
@@ -2,6 +2,6 @@ const menuBtn = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
 
 menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('is-active');
-    mobileNav.classList.toggle('is-active');
+  menuBtn.classList.toggle('is-active');
+  mobileNav.classList.toggle('is-active');
 });
---
+++ Trilha_ignite/Server/prisma/migrations/20230117115519_create_habits/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "crated_at" DATETIME NOT NULL
+);
